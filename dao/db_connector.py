721f08b33125386450db0ee975290174d2af379b
Ilya Epshteyn <i.a.epshtein@gmail.com>
2023-11-04 22:36:54 -0400

    connect to the DB

---
+++ dao/db_connector.py
@@ -0,0 +1,15 @@
+import psycopg2
+from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+
+conn = psycopg2.connect(database = DB_NAME,
+                        host = DB_HOST,
+                        port = DB_PORT,
+                        user = DB_USER,
+                        password = DB_PASSWORD)
+
+cursor = conn.cursor()
+
+
+def get_db_status():
+    cursor.execute("SELECT * FROM status WHERE id=1")
+    return cursor.fetchone()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
+from dao.db_connector import get_db_status
 
 app = Flask(__name__)
 
 @app.route("/status")
 def home():
-    return jsonify({'status': 'ok'}), 200
+    return jsonify(get_db_status()), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
