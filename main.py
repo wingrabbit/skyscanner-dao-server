909e00dd4d57be25cb1732ed42204cfcca75d595
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-02-02 21:20:54 -0500

    add cities post method processing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, jsonify
 from dao.db_connector import get_db_status
 from dao.orm_queries import search_city
@@ -8,13 +9,18 @@ app = Flask(__name__)
 def home():
     return jsonify(get_db_status()), 200
 
-@app.route("/city")
+@app.route("/city", methods=['GET', 'POST'])
 def city():
-    city = search_city(request.args.get("name"))
-    if city is not None:
-        return jsonify(city.__dict__), 200 
-    else: 
-        return jsonify({'id': -1}), 404
+    if request.method == 'GET':
+        city = search_city(request.args.get("name"))
+        if city is not None:
+            return jsonify(city.__dict__), 200 
+        else: 
+            return jsonify({'id': -1}), 404
+    elif request.method == 'POST':
+        data = request.get_json(force=True)
+        json_response = json.loads(data)
+        return json_response
 
 if __name__ == '__main__':
     app.run(debug=True)
