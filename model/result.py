2aaef361143a07922df8bc08b95a0bfed2d20c8e
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-02-02 20:26:58 -0500

    change result table structure

--- model/result.py
+++ model/result.py
@@ -3,11 +3,10 @@ from model.base_model import BaseModel
 class Result(BaseModel):
     def __init__(self, args) -> None:
         self.id = args[0]
-        self.city_from_id = args[1]
-        self.city_to_id = args[2]
-        self.date_from = args[3]
-        self.date_return = args[4]
-        self.last_updated = args[5]
+        self.search_id = args[1]
+        self.date_from = args[2]
+        self.date_return = args[3]
+        self.last_updated = args[4]
     
     def __str__(self) -> str:
-        return f'id={self.id}, {self.city_from_id}-{self.city_from_id} {self.date_from}-{self.date_return}, updated {self.last_updated}'
+        return f'id={self.id}, search={self.search_id} {self.date_from}-{self.date_return}, updated {self.last_updated}'
--- orm/results.py
+++ orm/results.py
@@ -6,5 +6,5 @@ class Results(BaseOrmModel):
     model = Result
     table_name = 'results'
 
-    default_fields = ['city_from_id', 'city_to_id', 'date_from', 'date_return']
+    default_fields = ['search_id', 'date_from', 'date_return']
     nullable_fields = ['last_updated']
