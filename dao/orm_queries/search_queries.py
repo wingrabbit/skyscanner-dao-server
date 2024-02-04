fba9005ce6e85e634d448f04e5a00a88cf1e7fc1
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-02-04 18:03:58 -0500

    implement search endpoint with user_id param

--- dao/orm_queries/search_queries.py
+++ dao/orm_queries/search_queries.py
@@ -5,3 +5,6 @@ from orm.searches import Searches, Search
 
 def insert_search(json_data):
     return Searches().insert_values([str(json_data["user_id"]),str(json_data["city_from_id"]),str(json_data["city_to_id"]),str(json_data["date_min"]),str(json_data["date_max"]),str(json_data["days_min"]),str(json_data["days_max"])])
+
+def get_searches_by_user_id(user_id):
+    return Searches().select_by_field('user_id', f'\'{user_id}\'', one_record=False)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
+from dataclasses import asdict
 from flask import Flask, request, jsonify
 from dao.db_connector import get_db_status
-from dao.orm_queries.search_queries import insert_search
+from dao.orm_queries.search_queries import insert_search, get_searches_by_user_id
 from dao.orm_queries.city_queries import search_city, insert_city
 from dao.orm_queries.result_queries import insert_result
 from dao.orm_queries.price_queries import insert_price
+from model.search import Search
 
 app = Flask(__name__)
 
@@ -28,6 +30,12 @@ def city():
 @app.route("/search", methods=['GET', 'POST'])
 def search():
     if request.method == 'GET':
+        user_id=request.args.get("user_id")
+        search = get_searches_by_user_id(user_id)
+        if search is not None:
+            result = json.dumps([ob.__dict__ for ob in search], indent=4, sort_keys=True, default=str)
+            print(result)
+            return json.loads(result), 200
         return jsonify({'id': -1}), 404
     elif request.method == 'POST':
         data = request.get_json(force=True)
