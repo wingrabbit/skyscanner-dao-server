a010aab76a2df7c7c02c12a6ea29138d0813be09
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-02-04 18:48:05 -0500

    add search by id/user_id endpoints

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dao.orm_queries.search_queries import insert_search, get_searches_by_user_i
 from dao.orm_queries.city_queries import search_city, insert_city
 from dao.orm_queries.result_queries import insert_result
 from dao.orm_queries.price_queries import insert_price
+from dao.db_connector import select_all_records, select_one_record
+from dao.sql_queries.view_queries import get_searches, get_search_by_id
 from model.search import Search
 
 app = Flask(__name__)
@@ -38,6 +40,14 @@ def search_by_user_id(user_id):
     search = get_searches_by_user_id(user_id)
     if search is not None:
         result = json.dumps([ob.__dict__ for ob in search], indent=4, sort_keys=True, default=str)
+        return json.loads(result), 200
+    return jsonify({'id': -1}), 404
+
+@app.route("/searches/<id>", methods=['GET'])
+def search_by_id(id):
+    search = select_one_record(get_search_by_id(id))
+    if search is not None:
+        result = json.dumps(search, indent=4, sort_keys=True, default=str)
         return json.loads(result), 200
     return jsonify({'id': -1}), 404
     
