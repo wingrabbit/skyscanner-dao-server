129689c48e99a663c00af47fd7b0e8a319c6b9a1
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-02-04 20:18:21 -0500

    add prices search/endpoint by search_id

--- dao/sql_queries/view_queries.py
+++ dao/sql_queries/view_queries.py
@@ -11,3 +11,22 @@ def get_searches():
 
 def get_search_by_id(id):
     return f"{get_searches()} WHERE s.id={id}"
+
+def get_top_prices_by_search_id(search_id, limit=15):
+    query = f"""WITH sub_q AS 
+            (SELECT
+            r.date_from,
+            r.date_return, 
+            (SELECT ci.name FROM cities ci WHERE s.city_from_id=ci.id) AS city_from, 
+            (SELECT ci.name FROM cities ci WHERE s.city_to_id=ci.id) AS city_to,
+            pr.price,
+            pr.stops
+            FROM prices pr
+            INNER JOIN results r ON pr.result_id=r.id
+            INNER JOIN searches s ON r.search_id=s.id
+            WHERE s.id={search_id}
+            ORDER BY stops, price ASC)
+        (SELECT * FROM sub_q WHERE stops=0 LIMIT {limit})
+        UNION ALL (SELECT * FROM sub_q WHERE stops=1 LIMIT {limit})
+        UNION ALL (SELECT * FROM sub_q WHERE stops=2 LIMIT {limit})"""
+    return query
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dao.orm_queries.city_queries import search_city, insert_city
 from dao.orm_queries.result_queries import insert_result
 from dao.orm_queries.price_queries import insert_price
 from dao.db_connector import select_all_records, select_one_record
-from dao.sql_queries.view_queries import get_searches, get_search_by_id
+from dao.sql_queries.view_queries import get_searches, get_search_by_id, get_top_prices_by_search_id
 from model.search import Search
 
 app = Flask(__name__)
@@ -43,11 +43,20 @@ def search_by_user_id(user_id):
         return json.loads(result), 200
     return jsonify({'id': -1}), 404
 
-@app.route("/searches/<id>", methods=['GET'])
-def search_by_id(id):
-    search = select_one_record(get_search_by_id(id))
+@app.route("/searches/<search_id>", methods=['GET'])
+def search_by_id(search_id):
+    search = select_one_record(get_search_by_id(search_id))
     if search is not None:
         result = json.dumps(search, indent=4, sort_keys=True, default=str)
+        return json.loads(result), 200
+    return jsonify({'id': -1}), 404
+
+@app.route("/searches/<search_id>/prices", methods=['GET'])
+def prices_by_search_id(search_id):
+    limit = request.args.get("limit")
+    search = select_all_records(get_top_prices_by_search_id(search_id)) if limit is None else select_all_records(get_top_prices_by_search_id(search_id, limit))
+    if search is not None:
+        result = json.dumps([ob for ob in search], indent=4, sort_keys=True, default=str)
         return json.loads(result), 200
     return jsonify({'id': -1}), 404
     
